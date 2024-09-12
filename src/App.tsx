52659e9f0af0ce0a966649ad232c5142a2c8e797
Legolas <xuanhau031297@gmail.com>
2024-09-12 15:29:07 +0700

    Test restful api

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { useEffect } from 'react'
 
 import useRouterElements from '@routes/useRouterElement'
 
-import { useGetToken } from '@hooks'
+import axios from 'axios'
 import { Toaster } from 'react-hot-toast'
 import './App.css'
 
 function App() {
   useEffect(() => {
     console.log('App mounted')
+    axios.get('https://api.restful-api.dev/objects').then((res) => {})
   }, [])
 
-  useGetToken()
+  // useGetToken()
   // useGetCurrentUser({ enabled: !!data?.token })
   // useGetMining({ enabled: !!data?.token })
   // useSyncData({ enabled: !!data?.token })
