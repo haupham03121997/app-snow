375cfa486cb27af89a7f92c7fe774db74da45c1f
Legolas <xuanhau031297@gmail.com>
2024-09-12 14:59:30 +0700

    Disabled devtool tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </BrowserRouter>
 )
