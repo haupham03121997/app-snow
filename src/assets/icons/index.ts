73d2bbdd88f80709eb072e8625fcb5c0279da973
Legolas <xuanhau031297@gmail.com>
2024-09-12 14:56:46 +0700

    Add whale private image

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+import Coins from './Coins'
+import Friends from './Friends'
+import Hamster from './Hamster'
+import Info from './Info'
+import Mine from './Mine'
+import Settings from './Settings'
+
+export { Coins, Friends, Hamster, Info, Mine, Settings }
---
+++ src/assets/images/index.ts
@@ -0,0 +1,25 @@
+import airdropBgrBottom from './airdrop.png'
+import binanceLogo from './binance-logo.png'
+import dailyCipher from './daily-cipher.png'
+import dailyCombo from './daily-combo.png'
+import dailyReward from './daily-reward.png'
+import dollarCoin from './dollar-coin.png'
+import hamsterCoin from './hamster-coin.png'
+import hamsterExchange from './hamster-exchange.png'
+import mainCharacter from './main-character.png'
+import questionCoin from './question-coin.png'
+import whalePirate from './whale-pirate.png'
+
+export {
+  airdropBgrBottom,
+  binanceLogo,
+  dailyCipher,
+  dailyCombo,
+  dailyReward,
+  dollarCoin,
+  hamsterCoin,
+  hamsterExchange,
+  mainCharacter,
+  questionCoin,
+  whalePirate
+}
