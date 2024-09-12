4e7d54e2be5d75d973a6ebc1da9035d0ad816382
Legolas <xuanhau031297@gmail.com>
2024-09-12 14:43:40 +0700

    Add constants folder

---
+++ src/constants/queryKeys.ts
@@ -0,0 +1,8 @@
+export enum QueryKeys {
+  CURRENT_USER = 'current-user',
+  AUTH_COMBO = 'auth-combo',
+  AUTH_CARD = 'auth-card',
+  AUTH_MINING = 'auth-mining',
+  AUTH_INVITE_FRIENDS = 'auth-invite-friends',
+  AUTH_SYNC = 'auth-sync'
+}
