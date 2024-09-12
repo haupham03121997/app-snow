ed1212c14b035833188650fdaabf5f6106d7538a
Legolas <xuanhau031297@gmail.com>
2024-09-12 14:50:01 +0700

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+
+import { useGetCurrentUser, useGetMining, useGetToken, usePostSyncPoints, useSyncData } from '@hooks'
+import useRouterElements from '@routes/useRouterElement'
+
+import { PointsIncrement, SheetSyncPoints } from '@components'
+import { Toaster } from 'react-hot-toast'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log('App mounted')
+  }, [])
 
+  const { data } = useGetToken()
+  useGetCurrentUser({ enabled: !!data?.token })
+  useGetMining({ enabled: !!data?.token })
+  useSyncData({ enabled: !!data?.token })
+  usePostSyncPoints()
+
+  const routeElements = useRouterElements()
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {routeElements}
+      <Toaster
+        containerStyle={{
+          top: '10%'
+        }}
+        position='top-right'
+        toastOptions={{
+          className: 'custom-toast',
+          duration: 3000
+        }}
+      />
+      <SheetSyncPoints />
+      <PointsIncrement />
     </>
   )
 }
