e31221b7602359fde4e16e942b53869f47876aa4
hau.pham <hau.pham@tpisoftware.com>
2024-09-27 14:48:36 +0700

    Update bot name telegram

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = 'https://smc.dt.ciis.canon.com/api/'
 
-export const TELEGRAM_BOT_URL = 'https://t.me/hamtaro_crypto_bot'
+export const TELEGRAM_BOT_URL = 'https://t.me/hamtaro_coins_bot'
