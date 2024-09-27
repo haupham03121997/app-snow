641b9f133f3bb094e466ef9eab57bf8ca31a9d11
hau.pham <hau.pham@tpisoftware.com>
2024-09-27 09:25:23 +0700

    Update url for bot telegram

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = 'https://smc.dt.ciis.canon.com/api/'
 
-export const TELEGRAM_BOT_URL = 'https://t.me/cc_lua_ga_bot'
+export const TELEGRAM_BOT_URL = 'https://t.me/hamtaro_crypto_bot'
